0be95475e6d3874bcec48ec3ea765ce2a844c025
Tigran_A <tigran.apresyan13@gmail.com>
2018-10-11 01:12:21 +0400

    commit2

--- main.js
+++ main.js
@@ -8,16 +8,31 @@ app.set('views', __dirname + '/frontend');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
-	res.render('index', {title:'index'});
+	res.render('index', {title:'LUXURY WATCH BRANDS'});
 	res.end();
 });
 
 app.get('/brend1', (req, res)=>{
-	res.render('brend1', {title:'Rolex', db: db});
+	res.render('brend1', {title:'Rolex/LUXURY WATCH BRANDS', db: db});
 	res.end();
 });
 
 app.get('/brend2', (req, res)=>{
-	res.render('brend2', {title:'brend2', db: db});
+	res.render('brend2', {title:'Hublot/LUXURY WATCH BRANDS', db: db});
+	res.end();
+});
+
+app.get('/brend3', (req, res)=>{
+	res.render('brend3', {title:'Omega/LUXURY WATCH BRANDS', db: db});
+	res.end();
+});
+
+app.get('/brend4', (req, res)=>{
+	res.render('brend4', {title:'Cartier/LUXURY WATCH BRANDS', db: db});
+	res.end();
+});
+
+app.get('/brend5', (req, res)=>{
+	res.render('brend5', {title:'Bvlgari/LUXURY WATCH BRANDS', db: db});
 	res.end();
 });
